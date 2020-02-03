2db01f637a3f7f6ad3aaba18992e7005d4c9b346
natter1 <leanahtan@yahoo.de>
2020-02-03 20:23:17 +0100

    update

--- examples/example_pi88_to_pptx.py
+++ examples/example_pi88_to_pptx.py
@@ -6,17 +6,9 @@ def main():
     run()
 
 def run():
-    filename = '../resources/quasi_static_12000uN.tdm'
-    filename = '../resources/AuSn_Creep/1000uN 01 LC.tdm'
-    # measurements_path = '../resources/AuSn_Creep/'
-    # measurements_path = '../resources/creep_example/'
-    measurements_path = '../resources/d/'
-    # measurements_path = '../resources/dc/'
-    measurements_path = '../resources/delme/'
-    # measurements_path = '../resources/'
-    # measurements_path = '../resources/190829_Cu_400-867-03-Nr16/'
-
-    #measurement = PI88Measurement(filename)
+    filename = '..\\resources\\quasi_static_12000uN.tdm'
+    # measurements_path = '..\\resources\\creep_example\\'
+    measurements_path = '..\\resources\\'
 
     pptx = PI88ToPPTX(measurements_path, TemplateExample())
     pptx.create_title_slide()

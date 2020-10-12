8ddd1783e78270a44d1567667939a20ce31ef163
natter1 <njoehrmann@gmail.com>
2020-10-12 18:51:36 +0200

    version='0.0.1a05'

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (root_path / "README.rst").read_text()
 
 setup(
     name='pi88reader',
-    version='0.0.1a04',
+    version='0.0.1a05',
     packages=['pi88reader'],
     url='https://github.com/natter1/pi88reader.git',
     license='MIT',
